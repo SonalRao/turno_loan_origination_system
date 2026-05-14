c3358a7c123769a3d5d4850f93e4a03cdf1a1f62
sonal <sonalmrao@gmail.com>
2026-05-14 20:11:24 +0530

    Refactored loan decision numbers into constants

--- src/main/java/com/turno/loanOriginationSystem/service/LoanDecisionService.java
+++ src/main/java/com/turno/loanOriginationSystem/service/LoanDecisionService.java
@@ -7,12 +7,17 @@ import com.turno.loanOriginationSystem.repo.LoanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
 public class LoanDecisionService {
     private final LoanRepository loanRepository;
+    private static int MAX_RECENT_APPLICATIONS = 5;
+    private static int MAX_PREVIOUS_REJECTION = 3;
+    private static BigDecimal MAXIMUM_LOAN_APPLICATION_AMOUNT_THRESHOLD = BigDecimal.valueOf(1_000_000);
+    private static BigDecimal MINIMUM_LOAN_APPLICATION_AMOUNT_THRESHOLD = BigDecimal.valueOf(1_000_000);
 
     public ApplicationStatus profileEvaluation(LoanApplication loan
     ) {
@@ -26,35 +31,35 @@ public class LoanDecisionService {
         /*
          * Fraud-like behavior: if customer has too many applications recently
          */
-        if (recentApplications >= 5) {
+        if (recentApplications >= MAX_RECENT_APPLICATIONS) {
             return ApplicationStatus.UNDER_REVIEW;
         }
 
         /*
          * If customer has too many previous rejections
          */
-        if (previousRejectedLoans >= 3) {
+        if (previousRejectedLoans >= MAX_PREVIOUS_REJECTION) {
             return ApplicationStatus.REJECTED_BY_SYSTEM;
         }
 
         /*
          * In case of Large BUSINESS loans require manual review
          */
-        if (loan.getLoanType() == LoanType.BUSINESS && loan.getLoanAmount().doubleValue() > 1000000) {
+        if (loan.getLoanType() == LoanType.BUSINESS && loan.getLoanAmount().compareTo(MAXIMUM_LOAN_APPLICATION_AMOUNT_THRESHOLD)>0) {
             return ApplicationStatus.UNDER_REVIEW;
         }
 
         /*
          * Customer has a Good history and smaller loan
          */
-        if (previousApprovedLoans >= 2 && loan.getLoanAmount().doubleValue() <= 500000) {
+        if (previousApprovedLoans >= 2 && loan.getLoanAmount().compareTo(MINIMUM_LOAN_APPLICATION_AMOUNT_THRESHOLD)>0) {
             return ApplicationStatus.APPROVED_BY_SYSTEM;
         }
 
         /*
         * Customers with smaller loans are approved
          */
-        if(loan.getLoanAmount().doubleValue() <= 50000){
+        if(loan.getLoanAmount().compareTo(MINIMUM_LOAN_APPLICATION_AMOUNT_THRESHOLD)>0){
             return ApplicationStatus.APPROVED_BY_SYSTEM;
         }
         /*
